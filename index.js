e83eafbca584d290152d6b5772326ac3e54ed598
tayobonferejo <oluwatayogeorgeige@gmail.com>
2026-05-04 22:11:21 +0100

    webpack

---
+++ index.js
@@ -0,0 +1,25 @@
+console.log(__dirname)
+
+const os = require("os");
+const { relative } = require("path");
+
+// setInterval(() => {
+//     console.log("hello world")
+// }, 500) 
+
+console.log(module);
+
+const user = os.userInfo();
+console.log(user);
+console.log(`The System Uptime is ${os.uptime()} seconds`)
+
+console.log(require("os").uptime());
+
+const  currentOS = {
+    name: os.type(),
+    release: os.release(),
+     totalMem: os.totalmem(),
+    freemem: os.freemem()
+}
+
+console.log(currentOS); 
