2ef68462bc148564cfad819796201c8dc45833ea
tayobonferejo <oluwatayogeorgeige@gmail.com>
2026-05-13 23:54:17 +0100

    more node

--- module.js
+++ module.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const {readFileSync } = require("fs");
-const homePage = readFileSync("./index.html")
+const homePage = readFileSync("./navbar-app/index.html")
 const server = http.createServer(function(request, response) {
     const url = request.url;
 
