b678ac1d5121406bdf2f173a48047b556843aef8
tayobonferejo <oluwatayogeorgeige@gmail.com>
2026-05-14 21:15:14 +0100

    express

--- module.js
+++ module.js
@@ -1,6 +1,9 @@
 const http = require("http");
 const {readFileSync } = require("fs");
-const homePage = readFileSync("./navbar-app/index.html")
+const homePage = readFileSync("./navbar-app/index.html");
+const homeStyle = readFileSync("./navbar-app/styles.css");
+const homeImage = readFileSync("./navbar-app/logo.svg");
+const homeLogic = readFileSync("./navbar-app/browser-app.js");
 const server = http.createServer(function(request, response) {
     const url = request.url;
 
@@ -13,6 +16,24 @@ const server = http.createServer(function(request, response) {
     else if (url === "/about") {
         response.writeHead(200, {"content-type":"text/html"})
         response.write(`<h2> about page</h2>`);
+        response.end();
+    }
+
+     else if (url === "/styles.css") {
+        response.writeHead(200, {"content-type":"text/css"})
+        response.write(homeStyle);
+        response.end();
+    }
+
+      else if (url === "/logo.svg") {
+        response.writeHead(200, {"content-type":"image/svg+xml"})
+        response.write(homeImage);
+        response.end();
+    }
+
+      else if (url === "/browser-app.js") {
+        response.writeHead(200, {"content-type":"text/javascript "})
+        response.write(homeStyle);
         response.end();
     }
 
