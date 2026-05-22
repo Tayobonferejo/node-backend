e578ae80994a092554bdbcbe58cbb45193a3b64d
tayobonferejo <oluwatayogeorgeige@gmail.com>
2026-05-22 18:55:46 +0100

    express

--- express.js
+++ express.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const {products } = require("./data");
 
 app.get("/", (request,response) => {
-    response.json([{name: "john"}, {name: "susan"}])
+    response.send(`<h2>Home Page </h2><a href="/api/products">product</a>`)
+})
+
+app.get("/api/products",(request,response) => {
+    const newProducts = products.map((product) => {
+        const {id, name, image} = product;
+        return{id, name, image};
+    })
+    response.json(newProducts); 
 })
 
 app.listen(5000, () => {
