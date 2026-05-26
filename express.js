70fcd740be5990698f515d386df79a8eefe8587d
tayobonferejo <oluwatayogeorgeige@gmail.com>
2026-05-26 03:32:33 +0100

    express

--- express.js
+++ express.js
@@ -4,32 +4,22 @@ const app = express();
 const {products } = require("./data");
 const { request } = require("http");
 
-app.get("/", (request,response) => {
-    response.send(`<h2>Home Page </h2><a href="/api/products">product</a>`)
+
+const logger = (request, response, next) => {
+    const method = request.method;
+    const url = request.url;
+    const time = new Date().getFullYear();
+    console.log(method, url, time);
+    next();
+}
+app.get("/",logger , (request,response) => {
+
+    response.send(`<h2>Home Page </h2><a href="/api/products">product</a>`);
 })
-
-// app.get("/api/products",(request,response) => {
-//     // const newProducts = products.map((product) => {
-//     //     const {id, name, image} = product;
-//     //     return{id, name, image};
-//     // })
-
-//     const singleProduct = products.find((product) => product.id === 4)
-//     response.json(singleProduct); 
-// })
-
-app.get("api/products/productID/reviews/:reviewID", (request, response) => {
-    console.log(request.params)
-    response.send("Hello world")
+app.get("/about" , logger, (request, response) => {
+    response.send("about")
 })
-
-app.get("/api/v1/query", (request, response) => {
-    console.log(request.query)
-    response.send("Hello world");
-})
-
-
 
 app.listen(5000, () => {
-    console.log("Server is listening on port 5000....")
+  console.log('Server is listening on port 5000....')
 })
